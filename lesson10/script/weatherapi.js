35b77ba1716926299f435a6372e4b2d129831fa4
zil22002@byui.edu <zilimbolanoluthando1991@gmail.com>
2023-03-11 12:48:57 +0200

    adding js

--- lesson10/script/weatherapi.js
+++ lesson10/script/weatherapi.js
@@ -1,5 +1,5 @@
 let currentYear = new Date().getFullYear();
-document.querySelector(".last-modified").innerHTML = lastModified;
+document.querySelector(".current-year").innerHTML = currentYear;
 
 let lastModified = document.lastModified;
 document.querySelector(".last-modified").innerHTML = lastModified;
@@ -9,37 +9,39 @@ const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&unit=metric&appid=7fae69e9db7f6cc9a7268bfc37203f6b"
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Johannesburg,sa&unit=metric&appid=7fae69e9db7f6cc9a7268bfc37203f6b"
 
 async function apiFetch() {
   try {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      console.log(data); // this is for testing the call
+      displayResults(data); // this is for testing the call
       // displayResults(data);
-    } else {
+    }
+    else {
         throw Error(await response.text());
     }
-  } catch (error) {
-      console.log(error);
+  }
+  catch (error) {
+    console.log(error);
   }
 }
 
 function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
 
 
-    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-    const desc = weatherData.weather[0].descrtion;
+    const iconSrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
 
-    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("src", iconSrc);
     weatherIcon.setAttribute("alt", desc);
     captionDesc.textContent = capitalizeStr(desc);
 }
 
 function capitalize(word) {
-    const capitalized = word.charAat(0).toUpperCase() + word.slice(1);
+    const capitalized = word.charAt(0).toUpperCase() + word.slice(1);
     return capitalized;
 }
 
@@ -51,7 +53,7 @@ function capitalizeStr(string) {
         capitalized = split.join(" ");
     }
     else {
-        capitalize = capitalize(string);
+        capitalized = capitalize(string);
     }
     return capitalized;
 }
