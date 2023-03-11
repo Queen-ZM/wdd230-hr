1a6a753a22e9ad60d18a3029f33b19942cacc5b9
zil22002@byui.edu <zilimbolanoluthando1991@gmail.com>
2023-03-11 20:07:18 +0200

    appending

--- lesson10/script/weatherapi.js
+++ lesson10/script/weatherapi.js
@@ -1,68 +1,51 @@
-const currentYear = new Date().getFullYear();
-document.querySelector(".current-year").innerHTML = currentYear;
+const year = document.querySelector('#year');
+const lastModified = document.querySelector('#lastModified');
+lastModified.textContent = document.lastModified;
+year.textContent = `${new Date().getFullYear()}`;
 
-const lastModified = document.lastModified;
-document.querySelector(".last-modified").innerHTML = lastModified;
+const currentTemp = document.querySelector('#current-temp');
 
-
-const currentTemp = document.querySelector("#current-temp");
-
-const img = document.createElement("img");
-const figure = document.querySelector("figure");
+const img = document.createElement('img');
+const figure = document.querySelector('figure');
 figure.appendChild(img);
-const figcaption = document.createElement("figcaption");
+const figcaption = document.createElement('figcaption');
 figure.appendChild(figcaption);
-const captionDesc = document.querySelector("figcaption");
+const captionDesc = document.querySelector('figcaption');
 
-const weatherIcon = document.querySelector("#weather-icon");
+const cityName = "Johannecburg";
+const apiID = "7fae69e9db7f6cc9a7268bfc37203f6b";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiID}&units=imperial`;
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Johannesburg,sa&unit=metric&appid=7fae69e9db7f6cc9a7268bfc37203f6b"
-
-async function apiFetch() {
+async function apiFetch(){
   try {
     const response = await fetch(url);
     if (response.ok) {
-      const data = await response.json();
-      displayResults(data); // this is for testing the call
-      // displayResults(data);
-    }
-    else {
+        const data = await response.json();
+        console.log(data);
+        displayResults(data);
+    } else {
         throw Error(await response.text());
     }
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
   }
 }
 
-function displayResults(weatherData) {
+function displayResults(weatherData){
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-
-
-    const iconSrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
 
-    weatherIcon.setAttribute("src", iconSrc);
-    weatherIcon.setAttribute("alt", desc);
+
+    img.setAttribute('src', iconsrc);
+    img.setAttribute('alt', desc);
+
     captionDesc.textContent = desc.toUpperCase();
+    
 }
 
-function capitalize(word) {
-    const capitalized = word.charAt(0).toUpperCase() + word.slice(1);
-    return capitalized;
-}
+apiFetch(); //Call apiFetch function for the first time.
 
-function capitalizeStr(string) {
-    let capitalized;
-    if (string.includes(" ")) {
-        let split = string.split(" ");
-        split = split.map((word) => capitalize(word));
-        capitalized = split.join(" ");
-    }
-    else {
-        capitalized = capitalize(string);
-    }
-    return capitalized;
-}
 
-apiFetch();
+
+
