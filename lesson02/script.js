4d256905120d853427836f840bfa4b805d2921ab
zil22002@byui.edu <zilimbolanoluthando1991@gmail.com>
2023-03-16 14:46:13 +0200

    fix

--- lesson02/script.js
+++ lesson02/script.js
@@ -7,7 +7,18 @@ lastModified.textContent = document.lastModified;
 //Display on the screen the current year.
 year.textContent = `${new Date().getFullYear()}`;
 
-const year = document.querySelector('#year');
-const lastModified = document.querySelector('#lastModified');
-lastModified.textContent = document.lastModified;
-year.textContent = `${new Date().getFullYear()}`;
+const datefield = document.querySelector(".Currentdate");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-ZA", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+const menuButton = document.querySelector('#menuButton');
+
+menuButton.addEventListener('click', function() {
+    toggleMenu();
+});
+
+
